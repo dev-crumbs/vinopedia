80393719b25c8125d9a653446ccb9f9f056887c7
DevCrumbs <mail@devcrumbs.com>
2022-01-21 10:05:05 +0100

    separate label function

--- vini/Italia/Emilia/Medici-Ermete/Concerto/concerto.js
+++ vini/Italia/Emilia/Medici-Ermete/Concerto/concerto.js
@@ -43,11 +43,9 @@ function wineData(){
         .text(function(d){return(d.Sentore)})
         .attr("transform", function(d) { return (x(d.Sentore) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "rotate(180)" : "rotate(0)"; })
         .attr("x", function(d) { return (x(d.Sentore) + x.bandwidth() / 2 + Math.PI) % (2 * Math.PI) < Math.PI ? "-"+labelDistance : labelDistance; })
-        .style("font-size", "20px")
-        .style("font-weight", "900")
-        .attr("fill", colorScaleText)
-        .attr("alignment-baseline", "middle")
   
+      labelStyle();
+            
       const vpText = thisWine.append("g")
       .style("text-anchor","middle")
       .style("font-size", "20px")
