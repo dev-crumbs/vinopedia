f8fdf21546f7eda4842b37bf90e0a5ba57ba4605
DevCrumbs <mail@devcrumbs.com>
2022-01-21 22:10:46 +0100

    generalization 31

--- vini/Italia/Emilia/Medici-Ermete/Concerto/concerto.js
+++ vini/Italia/Emilia/Medici-Ermete/Concerto/concerto.js
@@ -9,7 +9,7 @@ function wineData(){
       const x = d3.scaleBand().range(xAxisRange).align(0).domain(data.map(d => d.Sentore));
       const y = d3.scaleRadial().range([vpInnerRadius, vpOuterRadius]).domain(yAxisDomain);
       //Add the bars
-      d3.select(`.vini > svg > g`).append("g")
+      d3.selectAll(`.vini > svg > g`).append("g")
         .selectAll("path")
         .data(data)
         .join("path")
@@ -76,3 +76,31 @@ window.addEventListener("load", function(){
 mainSvg()
 setTimeout(wineData, 1500)  
 });
+
+
+
+
+function wineText(doc, name, cantina, vitigno, punteggio, annoCorrente){
+  const vpText = d3.select(`.vini > svg > g`).append("g")
+    .style("text-anchor","middle")
+    .style("font-size", "20px")
+  vpText.append("text")
+    .text(doc)
+    .attr("dy", -50)  
+  vpText.append("text")
+    .text(name)
+    .attr("dy", -25)  
+    .style("font-style","italic")
+  vpText.append("text")
+    .text(cantina)
+    .attr("dy", 0)
+  vpText.append("text")
+    .text(annoCorrente)
+    .attr("dy", 25)
+  vpText.append("text")
+    .text(vitigno)
+    .attr("dy", 50)
+  vpText.append("text")
+    .text(punteggio)
+    .attr("dy", 75) 
+}
