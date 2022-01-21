f2b97e7f6166cf878d187624bc61f17a8031fb43
DevCrumbs <mail@devcrumbs.com>
2022-01-21 11:03:53 +0100

    tidy-test 2

--- vini/vini.js
+++ vini/vini.js
@@ -10,6 +10,7 @@ yAxisDomain = [0, 300];
 familiesOuterRadius = vpOuterRadius - 60
 familiesInnerRadius = vpOuterRadius - 40
 
+window.addEventListener("load", function(){
 const wineId = "#" + document.querySelector("#analisi-olfattiva").nextElementSibling.id;
 const thisWine = d3.select(`${wineId} svg g`) 
 
@@ -37,6 +38,7 @@ const vpText = function(doc, name, cantina, vitigno, punteggio){
     .text(punteggio)
     .attr("dy", 75)
 }
+});
 
 // Color scales
 const myColor = d3.scaleLinear().domain([0,1,2,3,4,5,6,7,8,9,10,11])
