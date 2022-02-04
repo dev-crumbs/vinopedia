0b91153b275e4b930ac0b66b454d6daf53b26643
DevCrumbs <mail@devcrumbs.com>
2022-02-04 19:09:41 +0100

    fattorie vallona 8

--- produttori/produttore-summary-table.js
+++ produttori/produttore-summary-table.js
@@ -1,7 +1,6 @@
 window.addEventListener("load", function(){
-  d3.csv("produttore-summary.csv", function(error, data) {
-    if (error) throw error;
-    
+  d3.csv("produttore-summary.csv").then( function(data) {
+  
     var sortAscending = true;
     var table = d3.select('#table-container').append('table').attr('class','produttore-summary');
     var titles = d3.keys(data[0]);
@@ -12,5 +11,36 @@ window.addEventListener("load", function(){
                      .text(function (d) {
                         return d;
                       })
+                     .on('click', function (d) {
+                       headers.attr('class', 'header');
+                       
+                       if (sortAscending) {
+                         rows.sort(function(a, b) { return b[d] < a[d]; });
+                         sortAscending = false;
+                         this.className = 'aes';
+                       } else {
+                       rows.sort(function(a, b) { return b[d] > a[d]; });
+                       sortAscending = true;
+                       this.className = 'des';
+                       }
+                       
+                     });
+    
+    var rows = table.append('tbody').selectAll('tr')
+                 .data(data).enter()
+                 .append('tr');
+    rows.selectAll('td')
+      .data(function (d) {
+        return titles.map(function (k) {
+          return { 'value': d[k], 'name': k};
+        });
+      }).enter()
+      .append('td')
+      .attr('data-th', function (d) {
+        return d.name;
+      })
+      .text(function (d) {
+        return d.value;
+      });
   });
   });
