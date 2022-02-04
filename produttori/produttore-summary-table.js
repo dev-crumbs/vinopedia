8dd5fadfe05508363981bad14671cd56320fe500
DevCrumbs <mail@devcrumbs.com>
2022-02-04 19:59:55 +0100

    fattorie vallona 10

--- produttori/produttore-summary-table.js
+++ produttori/produttore-summary-table.js
@@ -3,7 +3,7 @@ window.addEventListener("load", function(){
     
     var sortAscending = true;
     var table = d3.select('#table-container').append('table').attr('class','produttore-summary');
-    var titles = d3.keys(data[0]);
+    var titles = Object.keys(data[0]);
     var headers = table.append('thead').append('tr')
                      .selectAll('th')
                      .data(titles).enter()
