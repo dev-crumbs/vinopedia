34ba7de411c188e3d7f64409fca0c50998cb84ee
DevCrumbs <mail@devcrumbs.com>
2022-02-05 13:20:52 +0100

    js refactoring 5

--- js/table-filter.js
+++ js/table-filter.js
@@ -1,4 +1,5 @@
 
+if (window.location.href.indexOf("/cantina") != -1){
   window.addEventListener("load", function(){
     const totaleVini = document.querySelectorAll('[data-label="Pos."]').length;
     document.querySelector("#totalevini").innerText = totaleVini;
@@ -71,26 +72,27 @@
       });
     });
   });
+}
 
-  export function myFunction() {
-    var input, filter, table, tr, td, cell, i, j;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("sortMe");
-    tr = table.getElementsByTagName("tr");
-    for (i = 1; i < tr.length; i++) {
-      // Hide the row initially.
-      tr[i].style.display = "none";
-    
-      td = tr[i].getElementsByTagName("td");
-      for (var j = 0; j < td.length; j++) {
-        cell = tr[i].getElementsByTagName("td")[j];
-        if (cell) {
-          if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-            break;
-          } 
-        }
+export function myFunction() {
+  var input, filter, table, tr, td, cell, i, j;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("sortMe");
+  tr = table.getElementsByTagName("tr");
+  for (i = 1; i < tr.length; i++) {
+    // Hide the row initially.
+    tr[i].style.display = "none";
+  
+    td = tr[i].getElementsByTagName("td");
+    for (var j = 0; j < td.length; j++) {
+      cell = tr[i].getElementsByTagName("td")[j];
+      if (cell) {
+        if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+          break;
+        } 
       }
     }
   }
+}
