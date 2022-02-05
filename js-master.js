5ddff59cf8137a8442bf499385f4bf6b9d0d6bfe
DevCrumbs <mail@devcrumbs.com>
2022-02-05 10:48:51 +0100

    js refactoring 4

--- js-master.js
+++ js-master.js
@@ -1,5 +1,5 @@
 import { produttoreSummaryTable } from './it/js/produttore-summary-table.js';
-import { myFunction } from './it/js/tablefilter.js';
+import { myFunction } from './it/js/table-filter.js';
 
 if (window.location.href.indexOf("/produttori/") != -1){
   produttoreSummaryTable()
