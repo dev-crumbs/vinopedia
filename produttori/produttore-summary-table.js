f74c5f5f4db3516168f784dd18aa2fed84f1ba7b
DevCrumbs <mail@devcrumbs.com>
2022-02-05 07:07:54 +0100

    fattorie vallona 13

--- produttori/produttore-summary-table.js
+++ produttori/produttore-summary-table.js
@@ -1,4 +1,41 @@
 window.addEventListener("load", function(){
+  d3.text("produttore-summary.csv").then( function(data) {
+    var sortAscending = true;
+    var csv = d3.csvParse(data), allheaders = d3.csvParseRows(data)[0],
+    table = d3.select('#table-container').append('table').attr('class','produttore-summary');
+        var titles = Object.keys(data[0]);
+    console.log(titles)
+        var headers = table.append('thead').append('tr')
+                     .selectAll('th')
+                     .data(allheaders).enter()
+                     .append('th')
+                     .text(function (d) {
+                        return d;
+                      })
+
+    
+    
+    var rows = table.append('tbody').selectAll('tr')
+                 .data(csv).enter()
+                 .append('tr');
+    rows.selectAll('td')
+      .data(function (d) {
+        return allheaders.map(function (k) {
+          return { 'value': d[k], 'name': k};
+        });
+      }).enter()
+      .append('td')
+      .attr('data-th', function (d) {
+        return d.name;
+      })
+      .text(function (d) {
+        return d.value;
+      });
+  });
+  });
+
+/*
+window.addEventListener("load", function(){
   d3.csv("produttore-summary.csv").then( function(data) {
     var sortAscending = true;
     var table = d3.select('#table-container').append('table').attr('class','produttore-summary');
@@ -44,5 +81,5 @@ window.addEventListener("load", function(){
   });
   });
 
-
+*/
 
