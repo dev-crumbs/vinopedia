cd1c3e4d77f79723142ac7b463f3c77b194a3f9d
DevCrumbs <mail@devcrumbs.com>
2022-02-05 18:29:49 +0100

    js refactoring 15

--- js-master.js
+++ js-master.js
@@ -4,3 +4,9 @@ import { myFunction } from './it/js/table-filter.js';
 if (window.location.href.indexOf("/produttori/") != -1){
   produttoreSummaryTable()
 }
+
+if (window.location.href.indexOf("/cantina") != -1){
+  window.addEventListener("load", function(){
+      document.querySelector('#myInput').addEventListener('keyup', myFunction)
+  })
+}
