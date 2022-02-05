8fc8bde216c2aa1acf6f515b4c6683ce3228c793
DevCrumbs <mail@devcrumbs.com>
2022-02-05 06:31:11 +0100

    fattorie vallona 12

--- produttori/produttore-summary-table.js
+++ produttori/produttore-summary-table.js
@@ -1,10 +1,9 @@
 window.addEventListener("load", function(){
-  d3.csv("produttore-summary.csv", function(data) {
-    
+  d3.csv("produttore-summary.csv").then( function(data) {
     var sortAscending = true;
     var table = d3.select('#table-container').append('table').attr('class','produttore-summary');
-    var titles = Object.keys(data[0]);
-    var headers = table.append('thead').append('tr')
+        var titles = Object.keys(data[0]);
+        var headers = table.append('thead').append('tr')
                      .selectAll('th')
                      .data(titles).enter()
                      .append('th')
@@ -44,3 +43,6 @@ window.addEventListener("load", function(){
       });
   });
   });
+
+
+
