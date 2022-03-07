f6d5c028bdac3536b4f6978337f8a8eb35504af2
DevCrumbs <mail@devcrumbs.com>
2022-03-07 12:15:19 +0100

    new

--- js/cantina-table.js
+++ js/cantina-table.js
@@ -2,7 +2,7 @@ export function cantinaTableIn(){
   d3.text(`cantina-table-in.csv`).then( function(data) {
     var sortAscending = true;
     var csv = d3.csvParse(data), allheaders = d3.csvParseRows(data)[0],
-    table = d3.select('#table-in-container').append('table').attr('class','summary-table sort produttore-table');
+    table = d3.select('#table-in-container').append('table').attr('class','summary-table sort');
         var titles = Object.keys(data[0]);
         var headers = table.append('thead').append('tr')
                     .selectAll('th')
@@ -41,7 +41,7 @@ export function cantinaTableOut(){
   d3.text(`cantina-table-out.csv`).then( function(data) {
     var sortAscending = true;
     var csv = d3.csvParse(data), allheaders = d3.csvParseRows(data)[0],
-    table = d3.select('#table-out-container').append('table').attr('class','summary-table sort produttore-table');
+    table = d3.select('#table-out-container').append('table').attr('class','summary-table sort');
         var titles = Object.keys(data[0]);
         var headers = table.append('thead').append('tr')
                     .selectAll('th')
