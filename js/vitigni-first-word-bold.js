59d03e17b5a50d4e2404ddac1ff2074308991806
DevCrumbs <mail@devcrumbs.com>
2022-03-08 16:21:34 +0100

    bold first word description fix

--- js/vitigni-first-word-bold.js
+++ js/vitigni-first-word-bold.js
@@ -1,2 +1,4 @@
-const firstWordBold = document.querySelector(".caption").innerHTML.replace(/[^\s]*/, '<span class="description-bold">Vitigni</span>');
-document.querySelector(".caption").innerHTML = firstWordBold
+export function firstWordBold(){ 
+  const firstWordBold = document.querySelector(".caption").innerHTML.replace(/[^\s]*/, '<span class="description-bold">Vitigni</span>');
+  document.querySelector(".caption").innerHTML = firstWordBold
+}
