a35aaa141253cf8b1603887821340c8b6534132b
DevCrumbs <mail@devcrumbs.com>
2022-03-10 14:01:10 +0100

    import from csv test 7

--- js/scheda-globale-import.js
+++ js/scheda-globale-import.js
@@ -1,38 +1,11 @@
-export function schedaGlobaleImport(){
-  d3.text(`../../../../listone.csv`).then( function(data) {
-    var csv = d3.csvParse(data);
-    
-    /*
-    var allheaders = d3.csvParseRows(data)[0],
-    table = d3.select('#table-in-container').append('table').attr('class','summary-table sort');
-        var titles = Object.keys(data[0]);
-        var headers = table.append('thead').append('tr')
-                    .selectAll('th')
-                    .data(allheaders).enter()
-                    .append('th')
-                    .text(function (d) {
-                        return d;
-                      })
-    var rows = table.append('tbody').selectAll('tr')
-                .data(csv).enter()
-                .append('tr');
-    rows.selectAll('td')
-      .data(function (d) {
-        return allheaders.map(function (k) {
-          return { 'value': d[k], 'name': k};
-        });
-      }).enter()
-      .append('td')
-      .attr('data-th', function (d) {
-        return d.name;
-      })
-      .text(function (d) {
-        return d.value;
-      });
-    for (const i of document.querySelectorAll('.sort th')) {
-      i.setAttribute("scope","col");
-      i.classList.add("table__header");
-    }
-    */
+export function schedaGlobaleImport() {
+  //page headline input
+  const headlineFull = document.querySelector(".headline").innerText
+  d3.text(`../../../../listone.csv`).then(function(data) {
+      const csv = d3.csvParse(data);
+      const tipologia = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Tipologia;
+
+      d3.select('.caratteristiche li:nth-child(1)').append().text(" " + headlineFull);
+      d3.select('.caratteristiche li:nth-child(2)').append().text(" " + tipologia)
   });
 }
