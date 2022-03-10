8689a957b7d804de2d43717b621ba7307524675a
DevCrumbs <mail@devcrumbs.com>
2022-03-10 16:08:35 +0100

    import from csv test 14

--- js/scheda-globale-import.js
+++ js/scheda-globale-import.js
@@ -5,12 +5,16 @@ export function schedaGlobaleImport() {
       const csv = d3.csvParse(data);
       const nomeCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Nome;
       const regioneCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Regione;
+      const nazioneCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Nazione;
       const tipoCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Tipologia;
       const denominazioneCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Denominazione;
+      const menzioniCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Menzioni;
       const classificazioneCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Classificazione;
       const produttoreCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Produttore;
+      const valutazioneCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Valutazione;
       const punteggioMedioCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].punteggioMedio;
-      const composizioneCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Composizione;    
+      const composizioneCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Composizione;
+      const composizioneArray = composizioneCSV.split(' – ')    
       const affinamentoCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Affinamento;
       const alcolCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Alcol;
       const prezzoCSV = csv.filter(function(d) {return d.Nome == headlineFull[0] && d.Entry === "1"})[0].Prezzo;
@@ -21,9 +25,11 @@ export function schedaGlobaleImport() {
       d3.select('h1').append().text(nomeCSV)
       d3.select('.caratteristiche li:nth-child(1)').append().text(" " + nomeCSV)
       d3.select('.caratteristiche li:nth-child(2)').append().text(" " + tipoCSV)
-      d3.select('.caratteristiche li:nth-child(3)').append().text(" " + denominazioneCSV + " " + classificazioneCSV)
-      d3.select('.caratteristiche li:nth-child(4)').append().text(" " + produttoreCSV)
-      d3.select('.caratteristiche li:nth-child(5)').append().text(" " + composizioneCSV)
+      d3.select('.caratteristiche li:nth-child(3)').append().html(`<a href="/denominazioni/${nazioneCSV}/${regioneCSV}/${classificazioneCSV}-${denominazioneCSV.replaceAll(' ', '-')}">${denominazioneCSV} ${classificazioneCSV}</a> (${menzioniCSV})`)
+      d3.select('.caratteristiche li:nth-child(4)').append().html(`<a href="/produttori/${nazioneCSV}/${regioneCSV}/${produttoreCSV.replaceAll(' ', '-')}">${produttoreCSV}</a>`)
+      for (const i of abbinamentoArray) {
+        d3.select('.caratteristiche li:nth-child(5)').append().html(`<a href="/vitigno/${nazioneCSV}/${i.replaceAll(' ', '-').toLowerCase()}">${i}</a>`)
+      }
       d3.select('.caratteristiche li:nth-child(6)').append().text(" " + affinamentoCSV)
       d3.select('.caratteristiche li:nth-child(7)').append().text(" " + alcolCSV)
       d3.select('.caratteristiche li:nth-child(8)').append().text(" " + prezzoCSV)
@@ -31,7 +37,7 @@ export function schedaGlobaleImport() {
         d3.select('.abbinamento').append("li").text(i)
       }
       for (const i of annateCSVArray) {
-        d3.select('.annate').append("li").html(`${nomeCSV} <a href="vini/Italia/${regioneCSV}/${produttoreCSV.replaceAll(' ', '-')}/${nomeCSV.replaceAll(' ', '-')}/${i.Anno}">${i.Anno}</a> -- /100`)
+        d3.select('.annate').append("li").html(`${nomeCSV} <a href="/vini/${nazioneCSV}/${regioneCSV}/${produttoreCSV.replaceAll(' ', '-')}/${nomeCSV.replaceAll(' ', '-')}/${i.Anno}">${i.Anno}</a> -- <span class="${valutazioneCSV}"></span> -- ${punteggioMedioCSV}/100`)
       }
   });
 }
