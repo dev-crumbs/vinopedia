0da6ebe85002710641c84e9800f7614bc537ac90
DevCrumbs <mail@devcrumbs.com>
2022-03-16 11:55:10 +0100

    new table test 4

--- js/denominazione-summary-table.js
+++ js/denominazione-summary-table.js
@@ -8,7 +8,7 @@ export function denominazioneSummaryTable(){
     d3.text(`${denominazione}/${denominazioneTipoNome}.csv`).then( function(data) {
       var sortAscending = true;
       var csv = d3.csvParse(data), allheaders = d3.csvParseRows(data)[0],
-      table = d3.select(`#${denominazioneTipoNomeL} + .denominazioneTipo div.table-container`).append('table').attr('class','summary-table sort denominazione-table');
+      table = d3.select(`#${denominazioneTipoNomeL} + .denominazioneTipo div.table-container`).append('table').attr('class','sort denominazione-table');
           var titles = Object.keys(data[0]);
           var headers = table.append('thead').append('tr')
                       .selectAll('th')
