6955e02c25e2f695334fc67c8e608d92830f2e79
DevCrumbs <mail@devcrumbs.com>
2022-03-30 09:25:39 +0200

    import spumanti

--- js/scheda-globale-import.js
+++ js/scheda-globale-import.js
@@ -1,11 +1,13 @@
 export function schedaGlobaleImport() {
+  //Checks
   const tempCheck = document.querySelector(".caratteristiche")
+  const listCheck = document.querySelector("div[role='list'] .v-list-item__title")
+  const spumanteCheck = document.querySelector(".spumantizzazione")
   if (tempCheck == null){return;}
   //page headline input
   const headlineFull = document.querySelector(".headline").innerText.split(' – ')
   //console.log(headlineFull[0])
   //console.log(headlineFull[2])
-  const listCheck = document.querySelector("div[role='list'] .v-list-item__title")
   d3.text(`/vini/listone.csv`).then(function(data) {
       const csv = d3.csvParse(data);
       const filterGlobalCSV = function(d) {return d.Nome == headlineFull[0] && d.Produttore == headlineFull[2].replaceAll('é', 'e') && d.Entry === "1"}
@@ -27,6 +29,10 @@ export function schedaGlobaleImport() {
       const prezzoCSV = csv.filter(filterGlobalCSV)[0].Prezzo;
       const abbinamentoCSV = csv.filter(filterGlobalCSV)[0].Abbinamento;
       const abbinamentoArray = abbinamentoCSV.split(' – ')
+      //spumantizzazione
+      const millesimoCSV = csv.filter(filterGlobalCSV)[0].Millesimo;
+      const tecnicaCSV = csv.filter(filterGlobalCSV)[0].Tecnica;
+      const zuccheroCSV = csv.filter(filterGlobalCSV)[0].Zucchero;
       //singola annata
       const annateCSVArray = csv.filter(filterSingleCSV)
       const valutazioneCSV = csv.filter(filterSingleCSV)[0].Valutazione;
@@ -39,14 +45,18 @@ export function schedaGlobaleImport() {
       d3.select('.caratteristiche li:nth-child(4)').append().html(` <a href="/produttori/${nazioneCSV}/${regioneCSV}/${produttoreCSV.replaceAll("' ", '-').replaceAll(' ', '-')}">${produttoreCSV}</a>`)
       for (const i of composizioneArray) {
         const cleanVitigno = i.split(/( \d+)/)[0].replaceAll(' ', '-').replaceAll("'", "-").toLowerCase()
-        function nationCheck(){switch(cleanVitigno){case"cabernet-sauvignon":case"cabernet-franc":case"carmenere":return"Francia";case"pinot-nero":return"Francia";case"merlot":return"Francia";case"syrah":return"Francia";default:return"Italia"}}
+        function nationCheck(){switch(cleanVitigno){case"cabernet-sauvignon":case"cabernet-franc":case"carmenere":return"Francia";case"pinot-noir":return"Francia";case"merlot":return"Francia";case"syrah":return"Francia";case"sauvignon-blanc":return"Francia";case"chardonnay":return"Francia";case"pinot-blanc":return"Francia";case"kerner":return"Germania";default:return"Italia"}}
         if (!--composizioneLength){
           d3.select('.caratteristiche li:nth-child(5)').append().html(` <a href="/vitigni/${nationCheck()}/${cleanVitigno}">${i}</a>`)          
         } else {
           d3.select('.caratteristiche li:nth-child(5)').append().html(` <a href="/vitigni/${nationCheck()}/${cleanVitigno}">${i}</a> -`)
         }
       }
-      d3.select('.caratteristiche li:nth-child(6)').append().text(" " + affinamentoCSV)
+      if (spumanteCheck == null){      
+        d3.select('.caratteristiche li:nth-child(6)').append().text(" " + affinamentoCSV)
+      } else {
+
+      }
       d3.select('.caratteristiche li:nth-child(7)').append().text(" " + alcolCSV + "%") 
       d3.select('.caratteristiche li:nth-child(8)').append().text(" " + prezzoCSV + "€")
       for (const i of abbinamentoArray) {
@@ -56,8 +66,17 @@ export function schedaGlobaleImport() {
         d3.select('.annate').append("li").html(`${nomeCSV} <a href="/vini/${nazioneCSV}/${regioneCSV}/${produttoreCSV.replaceAll("' ", '-').replaceAll(' ', '-').replaceAll("'", '-')}/${nomeCSV.replaceAll(' ', '-').replaceAll("'", '-')}/${i.Anno}">${i.Anno}</a> -- <span class="star-${i.Valutazione}"></span> -- ${i.PunteggioMedio}/100`)
       }
       // add ittle to list
+      if (spumanteCheck == null){
+        
+      } else {
+        d3.select('.spumantizzazione li:nth-child(1)').append().text(" " + millesimoCSV)
+        d3.select('.spumantizzazione li:nth-child(2)').append().text(" " + tecnicaCSV)
+        d3.select('.spumantizzazione li:nth-child(3)').append().text(" " + zuccheroCSV)
+        d3.select('.spumantizzazione li:nth-child(4)').append().text(" " + affinamentoCSV)
+      }
       if (listCheck == null){return;} else {
         document.querySelector("div[role='list'] .v-list-item__title").innerText = headlineFull[0]
       }
+
   });
 }
