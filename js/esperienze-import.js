f20a9dbae95dadac486bbb9e5198fbe0545e8536
DevCrumbs <mail@devcrumbs.com>
2022-03-30 17:29:49 +0200

    esperienza 1 fix 2

--- js/esperienze-import.js
+++ js/esperienze-import.js
@@ -6,7 +6,7 @@ export function esperienzeImport(){
   d3.text(`${esperienza}.csv`).then( function(data) {
     var sortAscending = true;
     var csv = d3.csvParse(data), allheaders = d3.csvParseRows(data)[0],
-    table = d3.select(`div.table-container`).append('table').attr('class','sort esperienza-table');
+    table = d3.select(`div.esperienza-table-container`).append('table').attr('class','sort esperienza-table');
         var titles = Object.keys(data[0]);
         var headers = table.append('thead').append('tr')
                     .selectAll('th')
