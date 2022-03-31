39f378eb8c5f2112823013edcdb5f4a53dd969b2
DevCrumbs <mail@devcrumbs.com>
2022-03-31 06:24:32 +0200

    test single wine 2

--- js-master.js
+++ js-master.js
@@ -44,7 +44,7 @@ window.addEventListener("load", function(){
     firstWordBold()
   }
   // scheda globale import csv
-  if (window.location.href.indexOf("/vini/") != -1){
+  if (window.location.href.indexOf("/scheda-globale/") != -1){
     schedaGlobaleImport()
   }
   // scheda singola import csv
