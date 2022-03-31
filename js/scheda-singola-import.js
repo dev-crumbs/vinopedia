1491031502e57c505458c4b7dfeba0838f36598e
DevCrumbs <mail@devcrumbs.com>
2022-03-31 10:42:14 +0200

    test single wine 7

--- js/scheda-singola-import.js
+++ js/scheda-singola-import.js
@@ -1,3 +1,73 @@
 export function schedaSingolaImport() {
-  console.log("success")
+  //Checks
+  const listCheck = document.querySelector("div[role='list'] .v-list-item__title")
+  const spumanteCheck = document.querySelector(".spumantizzazione")
+  const noteCheck = document.querySelector(".note")
+  //page headline input
+  const headlineFull = document.querySelector(".headline").innerText.split(' – ')
+  const caption = document.querySelector(".is-page-header .caption").innerText
+  d3.text(`/vini/listone.csv`).then(function(data) {
+      const csv = d3.csvParse(data);
+      const filterCSV = function(d) {return d.Ref == caption && d.Anno == headlineFull[1]}
+      //tutte le annate
+      const nomeCSV = csv.filter(filterCSV)[0].Nome;
+      const regioneCSV = csv.filter(filterCSV)[0].Regione;
+      const nazioneCSV = csv.filter(filterCSV)[0].Nazione;
+      const tipoCSV = csv.filter(filterCSV)[0].Tipologia;
+      const denominazioneCSV = csv.filter(filterCSV)[0].Denominazione;
+      const menzioniCSV = csv.filter(filterCSV)[0].Menzioni;
+      const classificazioneCSV = csv.filter(filterCSV)[0].Classificazione;
+      const produttoreCSV = csv.filter(filterCSV)[0].Produttore;
+      const composizioneCSV = csv.filter(filterCSV)[0].Composizione;
+      const composizioneArray = composizioneCSV.split(' – ')    
+      let composizioneLength = composizioneArray.length; //must be let for the condition to work
+      const affinamentoCSV = csv.filter(filterCSV)[0].Affinamento;
+      const alcolCSV = csv.filter(filterCSV)[0].Alcol;
+      const prezzoCSV = csv.filter(filterCSV)[0].Prezzo;
+      const abbinamentoCSV = csv.filter(filterCSV)[0].Abbinamento;
+      const noteCSV = csv.filter(filterCSV)[0].Note;
+      //spumantizzazione
+      const millesimoCSV = csv.filter(filterCSV)[0].Millesimo;
+      const tecnicaCSV = csv.filter(filterCSV)[0].Tecnica;
+      const zuccheroCSV = csv.filter(filterCSV)[0].Zucchero;
+
+      d3.select('h1').append().text(nomeCSV)
+      d3.select('.caratteristiche li:nth-child(1)').append().text(" " + nomeCSV)
+      d3.select('.caratteristiche li:nth-child(2)').append().text(" " + tipoCSV)
+      d3.select('.caratteristiche li:nth-child(3)').append().html(` <a href="/denominazioni/${nazioneCSV}/${regioneCSV}/${classificazioneCSV}-${denominazioneCSV.replaceAll(' ', '-')}">${denominazioneCSV} ${classificazioneCSV}</a> | ${menzioniCSV}`)
+      d3.select('.caratteristiche li:nth-child(4)').append().html(` <a href="/produttori/${nazioneCSV}/${regioneCSV}/${produttoreCSV.replaceAll("' ", '-').replaceAll(' ', '-')}">${produttoreCSV}</a>`)
+      for (const i of composizioneArray) {
+        const cleanVitigno = i.split(/( \d+)/)[0].replaceAll(' ', '-').replaceAll("'", "-").toLowerCase()
+        function nationCheck(){switch(cleanVitigno){case"cabernet-sauvignon":case"cabernet-franc":case"carmenere":return"Francia";case"pinot-noir":return"Francia";case"merlot":return"Francia";case"syrah":return"Francia";case"sauvignon-blanc":return"Francia";case"chardonnay":return"Francia";case"pinot-blanc":return"Francia";case"kerner":return"Germania";case"muller-thurgau":return"Svizzera";default:return"Italia"}}
+        if (!--composizioneLength){
+          d3.select('.caratteristiche li:nth-child(5)').append().html(` <a href="/vitigni/${nationCheck()}/${cleanVitigno}">${i}</a>`)          
+        } else {
+          d3.select('.caratteristiche li:nth-child(5)').append().html(` <a href="/vitigni/${nationCheck()}/${cleanVitigno}">${i}</a> -`)
+        }
+      }
+      if (spumanteCheck == null){      
+        d3.select('.caratteristiche li:nth-child(6)').append().text(" " + affinamentoCSV)
+        d3.select('.caratteristiche li:nth-child(7)').append().text(" " + alcolCSV + "%") 
+        d3.select('.caratteristiche li:nth-child(8)').append().text(" " + prezzoCSV + "€")
+      } else {
+        d3.select('.caratteristiche li:nth-child(6)').append().text(" " + alcolCSV + "%")
+        d3.select('.caratteristiche li:nth-child(7)').append().text(" " + prezzoCSV + "€")
+      }
+
+      d3.select('.abbinamento').append("li").text(abbinamentoCSV)
+
+      // spumante
+      if (spumanteCheck == null){ } else {
+        d3.select('.spumantizzazione li:nth-child(1)').append().text(" " + millesimoCSV)
+        d3.select('.spumantizzazione li:nth-child(2)').append().text(" " + tecnicaCSV)
+        d3.select('.spumantizzazione li:nth-child(3)').append().text(" " + zuccheroCSV)
+        d3.select('.spumantizzazione li:nth-child(4)').append().text(" " + affinamentoCSV)
+      }
+      if (noteCheck == null){ } else{
+        d3.select('.note').append().text(noteCSV)
+      }
+      if (listCheck == null){return;} else {
+        document.querySelector("div[role='list'] .v-list-item__title").innerText = headlineFull[0]
+      }
+  });
 }
