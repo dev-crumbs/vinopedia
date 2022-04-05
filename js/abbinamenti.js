4a6f31effdd7d67ad977a17d8fcf6a78d6baa3cf
DevCrumbs <mail@devcrumbs.com>
2022-04-05 10:33:38 +0200

    abbinamenti test 15

--- js/abbinamenti.js
+++ js/abbinamenti.js
@@ -1,81 +1,65 @@
 export function abbinamentiTree(){
-  var margin = {top: 20, right: 90, bottom: 30, left: 90},
-    width = 660 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-// declares a tree layout and assigns the size
-var treemap = d3.tree()
-    .size([height, width]);
-
-// load the external data
-d3.csv("DOCG-Frascati-Superiore/abbinamenti.csv").then(function(flatData) {
-
-  // assign null correctly
-  flatData.forEach(function(d) {
-      if (d.parent == "null") { d.parent = null};
-    });
-
-  // convert the flat data into a hierarchy 
-  var treeData = d3.stratify()
-    .id(function(d) { return d.id; })
-    .parentId(function(d) { return d.parentId; })
-    (flatData);
-
-  // assign the name to each node
-  treeData.each(function(d) {
-      d.name = d.data.name;
-    });
-
-  //  assigns the data to a hierarchy using parent-child relationships
-  var nodes = d3.hierarchy(treeData, function(d) {
-    return d.children;
-    });
-
-  // maps the node data to the tree layout
-  nodes = treemap(nodes);
-
+  // set the dimensions and margins of the graph
+  const width = 900
+  const height = 1000
+  
   // append the svg object to the body of the page
-  // appends a 'group' element to 'svg'
-  // moves the 'group' element to the top left margin
-  var svg = d3.select(".abbinamenti").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom),
-    g = svg.append("g")
-      .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
-
-  // adds the links between the nodes
-  var link = g.selectAll(".link")
-    .data( nodes.descendants().slice(1))
-    .enter().append("path")
-    .attr("class", "link")
-    .attr("d", function(d) {
-       return "M" + d.y + "," + d.x
-       + "C" + (d.y + d.parent.y) / 2 + "," + d.x
-       + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x
-       + " " + d.parent.y + "," + d.parent.x;
-       });
-
-  // adds each node as a group
-  var node = g.selectAll(".node")
-    .data(nodes.descendants())
-    .enter().append("g")
-    .attr("class", function(d) { 
-      return "node" + 
-      (d.children ? " node--internal" : " node--leaf"); })
-    .attr("transform", function(d) { 
-      return "translate(" + d.y + "," + d.x + ")"; });
-
-  // adds the circle to the node
-  node.append("circle")
-    .attr("r", 10);
-
-  // adds the text to the node
-  node.append("text")
-    .attr("dy", ".35em")
-    .attr("x", function(d) { return d.children ? -13 : 13; })
-    .style("text-anchor", function(d) { 
-    return d.children ? "end" : "start"; })
-    .text(function(d) { return d.data.name; });
-});
-}
+  const svg = d3.select(".abbinamenti")
+    .append("svg")
+  //    .attr("width", width)
+    //  .attr("height", height)
+    .attr("viewBox", "0 0 800 600")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .append("g")
+      .attr("transform", "translate(10,0)");  // bit of margin on the left = 40
+  
+  // read json data
+  d3.json("DOCG-Frascati-Superiore/abbinamenti.json").then( function(data) {
+  
+    // Create the cluster layout:
+    const cluster = d3.cluster()
+      .size([height, width - 100]);  // 100 is the margin I will have on the right side
+  
+    // Give the data to this cluster layout:
+    const root = d3.hierarchy(data, function(d) {
+        return d.children;
+    });
+    cluster(root);
+  
+  
+    // Add the links between nodes:
+    svg.selectAll('path')
+      .data( root.descendants().slice(1) )
+      .join('path')
+      .attr("d", function(d) {
+          return "M" + d.y + "," + d.x
+                  + "C" + (d.parent.y + 50) + "," + d.x
+                  + " " + (d.parent.y + 50) + "," + d.parent.x // 50 and 150 are coordinates of inflexion, play with it to change links shape
+                  + " " + d.parent.y + "," + d.parent.x;
+                })
+      .style("fill", 'none')
+      .attr("stroke", '#ccc')
+  
+  
+    // Add a circle for each node.
+    svg.selectAll("g")
+        .data(root.descendants())
+        .join("g")
+        .attr("transform", function(d) {
+            return `translate(${d.y},${d.x})`
+        })
+        .append("circle")
+          .attr("r", 7)
+          .style("fill", "#69b3a2")
+          .attr("stroke", "transparent")
+          .style("stroke-width", 2)
+     svg.selectAll("g").append("text")
+  //      .data(root.descendants())
+              .style('font', '12px sans-serif')
+              .text(function(d) { return d.data.name; });
+    svg.selectAll("text")
+         .attr('text-anchor', "start")
+         .attr('transform', "translate(10,5)")
+         .style('background-color', 'white')
+  })
+  }
