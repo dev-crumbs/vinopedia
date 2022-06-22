4ef5b79319b064914057230a6dc87f0a864ef342
DevCrumbs <mail@devcrumbs.com>
2022-06-22 12:41:46 +0200

    scheda globale fix for vscore

--- js/scheda-globale-import.js
+++ js/scheda-globale-import.js
@@ -8,7 +8,8 @@ export function schedaGlobaleImport() {
   //page headline input
   const headlineFull = document.querySelector(".headline").innerText.split(' – ')
   const regioneH = headlineFull[3].split(' (')
-  d3.text(`/vini/${regioneH[0]}.csv`).then(function(data) {
+  const regioneHSan = regioneH[0].replaceAll(' ', '-').replaceAll("'", '-')
+  d3.text(`/vini/${regioneHSan}.csv`).then(function myFunction(data) {
       const csv = d3.csvParse(data);
       const filterGlobalCSV = function(d) {return d.Nome == headlineFull[0] && d.Produttore == headlineFull[2].replaceAll('é', 'e') && d.Entry === "1"}
       const filterSingleCSV = function(d) {return d.Nome == headlineFull[0] && d.Produttore == headlineFull[2].replaceAll('é', 'e') && d.Entry === "2"}
@@ -40,26 +41,32 @@ export function schedaGlobaleImport() {
       const punteggioMedioCSV = csv.filter(filterSingleCSV)[0].PunteggioMedio;
 
       d3.select('h1').append().text(nomeCSV)
-      d3.select('.caratteristiche li:nth-child(1)').append().text(" " + nomeCSV)
-      d3.select('.caratteristiche li:nth-child(2)').append().text(" " + tipoCSV)
-      d3.select('.caratteristiche li:nth-child(3)').append().html(` <a href="/denominazioni/${nazioneCSV}/${regioneCSV}/${classificazioneCSV}-${denominazioneCSV.replaceAll(' ', '-')}">${classificazioneCSV} ${denominazioneCSV} </a> | ${menzioniCSV}`)
-      d3.select('.caratteristiche li:nth-child(4)').append().html(` <a href="/produttori/${nazioneCSV}/${regioneCSV}/${produttoreCSV.replaceAll("' ", '-').replaceAll(' ', '-')}">${produttoreCSV}</a>`)
+      d3.select('.caratteristiche li:nth-child(1)').append("span").text(" " + nomeCSV)
+      d3.select('.caratteristiche li:nth-child(2)').append("span").text(" " + tipoCSV)
+      d3.select('.caratteristiche li:nth-child(3)').append("a").text(`${classificazioneCSV} ${denominazioneCSV}`)
+      d3.select('.caratteristiche li:nth-child(3) a').attr("href",`/denominazioni/${nazioneCSV}/${regioneCSV}/${classificazioneCSV}-${denominazioneCSV.replaceAll(' ', '-')}`)
+      d3.select('.caratteristiche li:nth-child(3)').append("span").text(` | ${menzioniCSV}`)
+      d3.select('.caratteristiche li:nth-child(4)').append("a").text(`${produttoreCSV}`)
+      d3.select('.caratteristiche li:nth-child(4) a').attr("href",`/produttori/${nazioneCSV}/${regioneCSV}/${produttoreCSV.replaceAll("' ", '-').replaceAll(' ', '-')}`)
       for (const i of composizioneArray) {
         const cleanVitigno = i.split(/( \d+)/)[0].replaceAll(' ', '-').replaceAll("'", "-").toLowerCase()
         function nationCheck(){switch(cleanVitigno){case"cabernet-sauvignon":return"Francia";case"cabernet-franc":return"Francia";case"carmenere":return"Francia";case"pinot-noir":return"Francia";case"merlot":return"Francia";case"syrah":return"Francia";case"sauvignon-blanc":return"Francia";case"chardonnay":return"Francia";case"pinot-blanc":return"Francia";case"kerner":return"Germania";case"muller-thurgau":return"Svizzera";default:return"Italia"}}
         if (!--composizioneLength){
-          d3.select('.caratteristiche li:nth-child(5)').append().html(` <a href="/vitigni/${nationCheck()}/${cleanVitigno}">${i}</a>`)          
+          d3.select('.caratteristiche li:nth-child(5)').append("a").text(`${i}`) 
+          d3.select('.caratteristiche li:nth-child(5) a').attr("href", `/vitigni/${nationCheck()}/${cleanVitigno}`) 
         } else {
-          d3.select('.caratteristiche li:nth-child(5)').append().html(` <a href="/vitigni/${nationCheck()}/${cleanVitigno}">${i}</a> -`)
+          d3.select('.caratteristiche li:nth-child(5)').append("a").text(`${i}`) 
+          d3.select('.caratteristiche li:nth-child(5) a').attr("href", `/vitigni/${nationCheck()}/${cleanVitigno}`)
+          d3.select('.caratteristiche li:nth-child(5)').append("span").text(" - ")
         }
       }
       if (spumanteCheck == null){      
-        d3.select('.caratteristiche li:nth-child(6)').append().text(" " + affinamentoCSV)
-        d3.select('.caratteristiche li:nth-child(7)').append().text(" " + alcolCSV + "%") 
-        d3.select('.caratteristiche li:nth-child(8)').append().text(" " + prezzoCSV + "€")
+        d3.select('.caratteristiche li:nth-child(6)').append("span").text(" " + affinamentoCSV)
+        d3.select('.caratteristiche li:nth-child(7)').append("span").text(" " + alcolCSV + "%") 
+        d3.select('.caratteristiche li:nth-child(8)').append("span").text(" " + prezzoCSV + "€")
       } else {
-        d3.select('.caratteristiche li:nth-child(6)').append().text(" " + alcolCSV + "%")
-        d3.select('.caratteristiche li:nth-child(7)').append().text(" " + prezzoCSV + "€")
+        d3.select('.caratteristiche li:nth-child(6)').append("span").text(" " + alcolCSV + "%")
+        d3.select('.caratteristiche li:nth-child(7)').append("span").text(" " + prezzoCSV + "€")
       }
 
       for (const i of abbinamentoArray) {
@@ -72,10 +79,10 @@ export function schedaGlobaleImport() {
       if (spumanteCheck == null){
         
       } else {
-        d3.select('.spumantizzazione li:nth-child(1)').append().text(" " + millesimoCSV)
-        d3.select('.spumantizzazione li:nth-child(2)').append().text(" " + tecnicaCSV)
-        d3.select('.spumantizzazione li:nth-child(3)').append().text(" " + zuccheroCSV)
-        d3.select('.spumantizzazione li:nth-child(4)').append().text(" " + affinamentoCSV)
+        d3.select('.spumantizzazione li:nth-child(1)').append("span").text(" " + millesimoCSV)
+        d3.select('.spumantizzazione li:nth-child(2)').append("span").text(" " + tecnicaCSV)
+        d3.select('.spumantizzazione li:nth-child(3)').append("span").text(" " + zuccheroCSV)
+        d3.select('.spumantizzazione li:nth-child(4)').append("span").text(" " + affinamentoCSV)
       }
       if (noteCheck == null){ } else{
         d3.select('.note').append().text(noteCSV)
@@ -83,15 +90,26 @@ export function schedaGlobaleImport() {
       if (listCheck == null){return;} else {
         document.querySelector("div[role='list'] .v-list-item__title").innerText = headlineFull[0]
       }
-  });
-  d3.text(`/denominazioni/${nazioneCSV}/${regioneCSV}/${classificazioneCSV}-${denominazioneCSV.replaceAll(' ', '-')}/${classificazioneCSV}-${denominazioneCSV.replaceAll(' ', '-')}.csv`).then(function(data) {
-    const csv = d3.csvParse(data);
-    const filterInDen = function(d) {return d.Vino == headlineFull[0] && d.Produttore == headlineFull[2].replaceAll('é', 'e')}
-    // retrieve qp and v
-    const VScore = csv.filter(filterInDen)[0].VScore;
-    const QP = csv.filter(filterInDen)[0].QP;
-
-    d3.select('.vscore').append().text(" " + VScore)
-    d3.select('.qp').append().text(" " + QP)
+    return nazioneCSV;
+  }).then(function myFunction(data) {;
+        //second csv
+      let nazione2 = headlineFull[3].split(" (")
+      let regione2 = nazione2[0].replaceAll(" ", "-").replaceAll("'", "-")
+      nazione2 = nazione2[1]
+      if(nazione2 == "IT)"){
+        nazione2 = "Italia"
+      }                           
+      const denominazioneFull2 = document.querySelector(".grid-list.caratteristiche li:nth-child(3) a").innerText.replaceAll(" ", "-").replaceAll("'", "-")
+      const denominazione2 = denominazioneFull2.split("-")
+      d3.text(`/denominazioni/${nazione2}/${regione2}/${denominazione2[0]}-${regione2}/${denominazioneFull2}.csv`).then(function(data) {
+       const csv2 = d3.csvParse(data);
+       const filterInDen = function(d) {return d.Vino == headlineFull[0] && d.Produttore == headlineFull[2]}
+       // retrieve qp and v
+       const VScore = csv2.filter(filterInDen)[0].VScore;
+       const QP = csv2.filter(filterInDen)[0].QP;
+  
+       d3.select('.vscore').append().text(" " + VScore)
+       d3.select('.qp').append().text(" " + QP)
+     })
   })
 }
